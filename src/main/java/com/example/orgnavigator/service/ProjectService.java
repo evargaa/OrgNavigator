ce9481108628546dd663cd5731527714853da28d
Endre Varga <vendere10@gmail.com>
2023-10-06 13:36:09 +0200

    Added delete / updateStatus method

--- src/main/java/com/example/orgnavigator/service/ProjectService.java
+++ src/main/java/com/example/orgnavigator/service/ProjectService.java
@@ -1,5 +1,6 @@
 package com.example.orgnavigator.service;
 
+import com.example.orgnavigator.model.Employee;
 import com.example.orgnavigator.model.Project;
 import com.example.orgnavigator.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProjectService {
@@ -34,5 +36,54 @@ public class ProjectService {
             e.printStackTrace();
         }
             return new ResponseEntity<>("Error occurred while making " + newProject.getTitle() + " project.", HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<Optional<Project>> findProjectById(Long id) {
+        try {
+            if (projectRepository.findById(id).isPresent()) {
+                return new ResponseEntity<>(projectRepository.findById(id), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> deleteProject(Long id) {
+        try {
+            if(projectRepository.findById(id).isPresent()){
+                projectRepository.deleteById(id);
+                return new ResponseEntity<>("Successfully deleted project", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Project not found please another correct ID", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Error occured while deleting project", HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> updateStatus(Long id, Project newStatus) {
+        try {
+            Optional<Project> optionalProject = projectRepository.findById(id);
+            if (optionalProject.isPresent()) {
+                Project existingProject = optionalProject.get();
+                String newStatusValue = newStatus.getStatus();
+                if (newStatusValue != null) {
+                    existingProject.setStatus(newStatusValue);
+                    projectRepository.save(existingProject);
+                    return new ResponseEntity<>("Status successfully updated", HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>("Status field cannot be null", HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>("Didn't find project by id", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Error occurred while updating status", HttpStatus.BAD_REQUEST);
     }
 }
