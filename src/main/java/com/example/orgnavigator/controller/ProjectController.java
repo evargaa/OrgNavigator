c8d73049895e4961fe8ee1b45116e27e9f05dbbb
evargaa <vendere10@gmail.com>
2023-10-13 12:18:00 +0200

    Controller return responseEntities

--- src/main/java/com/example/orgnavigator/controller/ProjectController.java
+++ src/main/java/com/example/orgnavigator/controller/ProjectController.java
@@ -17,30 +17,66 @@ public class ProjectController {
     ProjectService projectService;
 
     @GetMapping("all")
-    public ResponseEntity<List<Project>> allProjects(){
-        return projectService.allProjects();
+    public ResponseEntity<List<Project>> allProjects() {
+        List<Project> projects = projectService.allProjects();
+        return ResponseEntity.ok(projects);
     }
 
     @GetMapping("find/{id}")
-    public ResponseEntity<Optional<Project>> findProjectById(@PathVariable Long id) {
-        return projectService.findProjectById(id);
+    public ResponseEntity<?> findProjectById(@PathVariable Long id) {
+        try {
+            Optional<Project> project = Optional.ofNullable(projectService.findProjectById(id));
+            if (project.isPresent()) {
+                return ResponseEntity.ok(project.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            String message = e.getMessage();
+            return ResponseEntity.badRequest().body(message);
+        }
     }
 
+
     @PostMapping("add")
-    public ResponseEntity<String> addNewProject(@RequestBody Project newProject){
-        return projectService.addNewProject(newProject);
+    public ResponseEntity<String> addNewProject(@RequestBody Project newProject) {
+        String message = projectService.addNewProject(newProject);
+        return ResponseEntity.ok(message);
     }
 
     @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deleteProject(@PathVariable Long id) {
-        return projectService.deleteProject(id);
+        String message;
+        try {
+            message = projectService.deleteProject(id);
+            return ResponseEntity.badRequest().body(message);
+        } catch (Exception e) {
+            message = e.getMessage();
+            return ResponseEntity.badRequest().body(message);
+        }
     }
 
     @PutMapping("updateStatus/{id}")
     public ResponseEntity<String> updateStatus(@PathVariable Long id, @RequestBody Project newStatus) {
-        return projectService.updateStatus(id, newStatus);
+        String message;
+        try {
+            message = projectService.updateStatus(id, newStatus);
+            return ResponseEntity.ok(message);
+        } catch (Exception e) {
+            message = e.getMessage();
+            return ResponseEntity.badRequest().body(message);
+        }
     }
 
-
-
+    @PutMapping("addEmployee/{id}")
+    public ResponseEntity<String> addEmployee(@PathVariable Long id) {
+        String message;
+        try {
+            message = projectService.addEmployee(id);
+            return ResponseEntity.ok(message);
+        } catch (Exception e) {
+            message = e.getMessage();
+            return ResponseEntity.badRequest().body(message);
+        }
+    }
 }
