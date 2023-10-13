e9dc33597c3a55e0b3f19ac2c40291fe50e64f23
evargaa <vendere10@gmail.com>
2023-10-13 12:47:58 +0200

    Service returns data

--- src/main/java/com/example/orgnavigator/service/PartnersService.java
+++ src/main/java/com/example/orgnavigator/service/PartnersService.java
@@ -15,32 +15,31 @@ import java.util.List;
 public class PartnersService {
 
     @Autowired
-    PartnersRepository partnersRepository;
+    private PartnersRepository partnersRepository;
 
-    public ResponseEntity<List<Partners>> allPartners() {
-        return new ResponseEntity<>(partnersRepository.findAll(), HttpStatus.OK);
+    public List<Partners> getAllPartners() {
+        return partnersRepository.findAll();
     }
 
-    public ResponseEntity<String> addPartner(Partners newPartner) {
+    public String addPartner(Partners newPartner) {
         partnersRepository.save(newPartner);
-        return new ResponseEntity<>("Successfully added new partner!", HttpStatus.CREATED);
+        return "Successfully added new partner!";
     }
 
-    public ResponseEntity<Partners> findPartnerByName(String name) {
+    public Partners findPartnerByName(String name) {
         Partners partner = partnersRepository.findByNameContaining(name);
-        if(partner == null){
+        if (partner == null) {
             throw new PartnerException("Partner not found with this name: " + name);
-        } else {
-            return new ResponseEntity<>(partner, HttpStatus.OK);
         }
+        return partner;
     }
 
-    public ResponseEntity<String> deletePartner(Long id) {
+    public String deletePartner(Long id) {
         if (partnersRepository.findById(id).isEmpty()) {
             throw new PartnerException("Partner not found with ID: " + id);
-        } else {
-            partnersRepository.deleteById(id);
-            return new ResponseEntity<>("Successfully deleted partner", HttpStatus.OK);
         }
+        partnersRepository.deleteById(id);
+        return "Successfully deleted partner";
     }
 }
+
