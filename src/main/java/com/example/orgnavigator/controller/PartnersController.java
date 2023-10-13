1ef94e1e88befefa3f5b38c274ca5b7bf321f92f
evargaa <vendere10@gmail.com>
2023-10-13 12:45:10 +0200

    Controller returns ResponseEntity

--- src/main/java/com/example/orgnavigator/controller/PartnersController.java
+++ src/main/java/com/example/orgnavigator/controller/PartnersController.java
@@ -4,6 +4,7 @@ package com.example.orgnavigator.controller;
 import com.example.orgnavigator.model.Partners;
 import com.example.orgnavigator.service.PartnersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,27 +15,34 @@ import java.util.List;
 public class PartnersController {
 
     @Autowired
-    PartnersService partnersService;
+    private PartnersService partnersService;
 
     @GetMapping("all")
     public ResponseEntity<List<Partners>> allPartners() {
-        return partnersService.allPartners();
+        List<Partners> partners = partnersService.getAllPartners();
+        return ResponseEntity.ok(partners);
     }
 
     @PostMapping("add")
-    public ResponseEntity<String> addPartner(@RequestBody Partners newPartner){
-        return partnersService.addPartner(newPartner);
+    public ResponseEntity<String> addPartner(@RequestBody Partners newPartner) {
+        String message = partnersService.addPartner(newPartner);
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+        }
     }
-
 
     @GetMapping("findByName/{name}")
     public ResponseEntity<Partners> findPartnerByName(@PathVariable String name) {
-        return partnersService.findPartnerByName(name);
+        Partners partner = partnersService.findPartnerByName(name);
+        return ResponseEntity.ok(partner);
     }
 
     @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deletePartner(@PathVariable Long id) {
-        return partnersService.deletePartner(id);
+        String message = partnersService.deletePartner(id);
+        return ResponseEntity.ok(message);
     }
-
 }
